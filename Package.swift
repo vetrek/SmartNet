04ebfa79db32c312870af5427f1e51efb98cfcd6
Valerio <listamaster@gmail.com>
2024-05-02 11:59:30 +0200

    update min macOS version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SmartNet",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15)
+        .macOS(.v10_14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
